54b4905ec784a3e52452f2b1a0d7e7d2fe21db24
plejon <per.lejon@netent.com>
2020-06-09 09:12:49 +0200

    setup.py, pre-commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="",
+    author="",
+    author_email="",
+    url="",
+    version="",
+    packages=[""],
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    long_description=open("README.md").read(),
+    install_requires=["setuptools>=38.4.0"],
+)
